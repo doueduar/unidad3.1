20cd2c5005931505548401afc1e7751f4918d32b
dou <doueduar12@gmail.com>
2021-06-09 07:53:14 -0600

    agrege el ejercicio5 java

---
+++ Java/Ejercicio5.java
@@ -0,0 +1,11 @@
+package Java;
+import java.util.*;
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("ingrese un numero");
+        int num1 = scanner.nextInt();
+        num1++;
+        System.out.println("El numero siguiente es: "+num1);
+    }
+}
