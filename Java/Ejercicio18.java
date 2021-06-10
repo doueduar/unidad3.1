cad71fc5f1859ad75cb70409b080bcb6ca16702d
dou <doueduar12@gmail.com>
2021-06-09 19:13:18 -0600

    nueva modificacion al ejercicio18

--- Java/Ejercicio18.java
+++ Java/Ejercicio18.java
@@ -10,24 +10,28 @@ public class Ejercicio18 {
         System.out.println("ingresa el el numero");
         numero= scanner.next();
         //System.out.println(numero.length());
-        if ((numero.length()<5)&&(0<numero.length())) {
+        d1 = 0; d2 =0; d3 =0; d4 =0;
 
-            d2 = Integer.parseInt(numero.substring(0, 1));
-           
-            d3 = Integer.parseInt(numero.substring(1, 2));
-            
-            d4 = Integer.parseInt(numero.substring(2, 3));
-            if (numero.length() == 2) {
-                d2 = 0;
+        if ((numero.length()<5)&&(0<numero.length())) {
+            if (1==numero.length()) {
+                d4 = Integer.parseInt(numero.substring(0,1));
             }
-            if (numero.length() == 1) {
-                d3 =0;
+            if (2==numero.length()) {
+                d4 = Integer.parseInt(numero.substring(1,2));
+                d3 = Integer.parseInt(numero.substring(0,1));
             }
-            if (numero.length()==3) {
-                d1 = 0;
-            }else{
-                d1 = Integer.parseInt(numero.substring(3, 4));
+            if (3==numero.length()) {
+                d2 = Integer.parseInt(numero.substring(0, 1));
+                d3 = Integer.parseInt(numero.substring(1,2));
+                d4 = Integer.parseInt(numero.substring(2,3));
             }
+            if (4 ==numero.length()) {
+                d1 = Integer.parseInt(numero.substring(0,1));
+                d2 = Integer.parseInt(numero.substring(1,2));
+                d3 = Integer.parseInt(numero.substring(2,3));
+                d4 = Integer.parseInt(numero.substring(3,4));
+            }
+
             suma = d1+d2+d3+d4;
             System.out.println("d1: "+d1+" d2: "+d2+" d3: "+d3+" d4: "+d4);
             System.out.println("la suma es "+suma);
